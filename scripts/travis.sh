7ec9834e9a4b11a2124fc042d94226845ff654c9
刘宁 <liuning@qutoutiao.net>
2019-10-12 20:29:01 +0800

    internal/config

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,4 +2,4 @@
 # 禁止部署配置文件
 rm -rf configs/douyacun.yml
 cd .. && rsync -azv api.douyacun.com/ douyacun:${deploy_dir}
-ssh douyacun "export _DOUYACUN_CONF='/data/www/api.douyacun.com/configs/douyacun.yml' && ${deploy_dir}/bin/douyacun stop && ${deploy_dir}/bin/douyacun start"
+ssh douyacun "export _DOUYACUN_CONF='${deploy_dir}/configs/douyacun.yml' && ${deploy_dir}/bin/douyacun stop && ${deploy_dir}/bin/douyacun start"
