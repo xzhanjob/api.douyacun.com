c10b1547a88fcebd2236994bfa99c77225223665
刘宁 <liuning@qutoutiao.net>
2019-12-29 23:48:23 +0800

    ci调整测试

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,4 +3,3 @@
 rm -rf configs/douyacun.yml
 rsync -azv ./* douyacun:${deploy_dir}
 ssh douyacun "export _DOUYACUN_CONF='${deploy_dir}/configs/douyacun.yml' && ${deploy_dir}/bin/douyacun stop && ${deploy_dir}/bin/douyacun start"
-curl http://www.douyacun.com/ping
