9c2c1bf718265598408068795cd1a436a4fb75e7
douyacun <douyacun@gmail.com>
2020-03-10 10:45:17 +0800

    cookie httponly

--- internal/module/account/main.go
+++ internal/module/account/main.go
@@ -234,11 +234,11 @@ func (a *Account) SetCookie(ctx *gin.Context) {
 	if err != nil {
 		panic(errors.Wrap(err, "set cookie json encode failed"))
 	}
-	ctx.SetCookie(consts.CookieName, string(cookie), 604800, "/", ".douyacun.com", false, false)
+	ctx.SetCookie(consts.CookieName, string(cookie), 604800, "/", ".www.douyacun.com", false, false)
 }
 
 func (a *Account) ExpireCookie(ctx *gin.Context) {
-	ctx.SetCookie(consts.CookieName, "", -1, "/", ".douyacun.com", false, true)
+	ctx.SetCookie(consts.CookieName, "", -1, "/", ".www.douyacun.com", false, false)
 }
 
 func (c *Cookie) VerifyCookie() bool {
