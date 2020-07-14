b23f7f1c516737b18092c952d154e31f0cf6500c
liuning <douyacun@gmail.com>
2020-07-14 11:00:54 +0800

    inet_ntoa

--- internal/module/util/ip.go
+++ internal/module/util/ip.go
@@ -57,6 +57,5 @@ func LocationByIp(ip string) (map[string]string, error) {
 }
 
 func InetNtoA(ip int64) string {
-	return fmt.Sprintf("%d.%d.%d.%d",
-		byte(ip>>24), byte(ip>>16), byte(ip>>8), byte(ip))
+	return fmt.Sprintf("%d.%d.%d.%d", byte(ip>>24), byte(ip>>16), byte(ip>>8), byte(ip))
 }
